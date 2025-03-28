bb63d2be26863ca611d24c1ed0e7b2a5022114b9
Diogo Castro <diogocastro.blv@gmail.com>
2025-03-28 03:00:10 -0300

    correção dos paths dos componentes

---
+++ simulador-lucro/src/App.jsx
@@ -0,0 +1,9 @@
+import SimuladorFinanceiro from "./SimuladorFinanceiro";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SimuladorFinanceiro />
+    </div>
+  );
+}
